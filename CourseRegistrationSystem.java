38c0931d0eecb144d928161b77b6a324db6c5f61
vikkikumar07 <vikkikumar2201@gmail.com>
2026-07-16 08:40:02 +0530

    view course function creat

--- CourseRegistrationSystem.java
+++ CourseRegistrationSystem.java
@@ -18,7 +18,68 @@ public class CourseRegistrationSystem {
         courses.add(new Course("W003", "Web Development", "HTML, CSS, JavaScript", 10));
         courses.add(new Course("A006", "App Development", "java, Kotiln", 10));
 
+        boolean exit = true;
+        while(exit) {
+            System.out.println("\n ===== Student Course Registration System =====");
+            System.out.println("1. view Courses");
+            System.out.println("2. Register Course");
+            System.out.println("3. Remove Course");
+            System.out.println("4. View Registered Courses");
+            System.out.println("5. Exit");
+
+            System.out.print("Enter Choice : ");
+            int choice = sc.nextInt();
+
+            switch(choice) {
+                case 1: 
+                    viewCourse();
+                    break;
+                case 2:
+                    registerCourse();
+                    break;
+                case 3:
+                    removeCourse();
+                    break;
+                case 4:
+                    viewRegisteredCourse();
+                    break;
+                case 5:
+                    System.out.println("Thank You !");
+                    exit = false;
+                    break;
+                default :
+                    System.out.println("Invalid Choice!");
+            }
+
+        }
+    }
+
+    // view course function
+    static void viewCourse() {
+        System.out.println("\nAvailable Courses");
+        for(Course c : courses) {
+            System.out.println("\n -----------------------------------");
+            System.out.println("Course Code : "+ c.code);
+            System.out.println("Course Name : "+ c.title);
+            System.out.println("Course Description : "+ c.description);
+            System.out.println("Available Slote : "+ c.getAvailableSlots());
+        }
+    }
+
+    // register course function
+    static void registerCourse() {
+
+    }
+
+    // remove course function
+    static void removeCourse() {
         
+    }
+
+    // view registered Course function
+    static void viewRegisteredCourse() {
+
     }
     
 }
+
